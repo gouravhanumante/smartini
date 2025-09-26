ffddd4b81c4bb75339478522e8a01fb9d0028296
Hanumante, Gourav CTIPD-TPA <gourav.hanumante+shell@capillarytech.com>
2025-09-27 02:17:31 +0530

    button ui fix result screen

--- app/src/main/java/com/dailyrounds/quizapp/ui/screen/ResultScreen.kt
+++ app/src/main/java/com/dailyrounds/quizapp/ui/screen/ResultScreen.kt
@@ -2,6 +2,7 @@ package com.dailyrounds.quizapp.ui.screen
 
 import android.content.res.Configuration
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -22,6 +23,10 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalConfiguration
@@ -154,17 +159,24 @@ fun ResultScreen(
             modifier = Modifier.padding(bottom = 10.dp),
             textAlign = TextAlign.Center
         )
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            ButtonStore.SecondaryButton(
+                text = "Restart Quiz",
+                onClick = onRestartQuiz,
+                modifier = Modifier.weight(1f)
+            )
 
-        ButtonStore.SecondaryButton(
-            text = "Restart Quiz",
-            onClick = onRestartQuiz
-        )
+            ButtonStore.PrimaryButton(
+                text = "Go To Home",
+                onClick = onHome,
+                modifier = Modifier.weight(1f)
+            )
+        }
 
-        Spacer(Modifier.height(12.dp))
 
-        ButtonStore.PrimaryButton(
-            text = "Go To Home",
-            onClick = onHome
-        )
     }
 }
